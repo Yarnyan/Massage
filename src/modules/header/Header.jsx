f956ad45e73457ffd55e429c985f770f46eeafe7
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-06-27 21:51:39 +0500

    Add files via upload

---
+++ src/modules/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './Header.module.scss'
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <div className={styles.container}>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ39GYQMHoEremuoU0-YwZsQVG2cKneeARLvw&s" alt="" />
+            </div>
+            <div className={styles.nav}>
+                <ul>
+                    <li>
+                        <a href="">О девочках</a>
+                    </li>
+                    <li>
+                        <a href="">Запись</a>
+                    </li>
+                    <li>
+                        <a href="">Что то еще</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+  )
+}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from '../modules/header/Header'
+export default function MainPage() {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
