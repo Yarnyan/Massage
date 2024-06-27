a7452f7edcc18a7875a0253d4d82f62a2254f92e
Harytan <92979595+Yarnyan@users.noreply.github.com>
2024-06-27 23:56:57 +0500

    update

--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Header.module.scss'
 export default function Header() {
+    
   return (
     <header className={styles.header}>
         <div className={styles.container}>
