8ea63fb7c2219624c7e40cfa296d677047104e54
Harytan <92979595+Yarnyan@users.noreply.github.com>
2024-06-28 01:23:55 +0500

    update

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../modules/header/Header'
+import Header from '../components/header/Header'
 export default function MainPage() {
   return (
     <div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,17 @@
-let
+import { configureStore, combineReducers,  } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { postApi } from '../api/postApi';
+
+const rootReducer = combineReducers({
+    [postApi.reducerPath]: postApi.reducer
+})
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(postApi.middleware),
+})
+
+setupListeners(store.dispatch);
+
+export default store;
